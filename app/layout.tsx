6514e44620bf4077189fe2bd415ccc7cb69a7eb7
rz_yang <yrzcode@hotmail.com>
2025-06-15 23:12:29 +0800

    create main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import Footer from "@/components/layouts/Footer";
+import Header from "@/components/layouts/Header";
+import Sidebar from "@/components/layouts/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,13 +29,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`flex h-screen ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header>
-          <h1>Header</h1>
-        </header>
-        <main>{children}</main>
-        <footer>Footer</footer>
+        {/* Sidebar Area */}
+        <aside className="w-40 h-full bg-amber-300 flex">
+          <Sidebar />
+        </aside>
+
+        {/* Main Area */}
+        <main className="flex-1 flex flex-col">
+          <header className="h-16 bg-amber-500">
+            <Header />
+          </header>
+          {/* Pages */}
+          <section className="flex-1 bg-amber-200">{children}</section>
+          <footer className="h-8 bg-amber-500">
+            <Footer />
+          </footer>
+        </main>
       </body>
     </html>
   );
