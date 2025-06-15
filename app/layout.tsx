643a68f790b9be969d010617941d3e8dc2d45a13
rz_yang <yrzcode@hotmail.com>
2025-06-15 14:05:21 +0800

    clear up project

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <header>
+          <h1>Header</h1>
+        </header>
+        <main>{children}</main>
+        <footer>Footer</footer>
       </body>
     </html>
   );
