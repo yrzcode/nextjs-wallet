e27fe9830a03c64bf475b5a9520f36ccbc17c39c
rz_yang <yrzcode@hotmail.com>
2025-06-21 22:46:26 +0800

    add logo redirect

--- components/layouts/AppSiderbar/Header.tsx
+++ components/layouts/AppSiderbar/Header.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 import { SidebarHeader } from "@/components/ui/sidebar";
 
 const Header = () => {
   return (
     <SidebarHeader className="grid place-items-center  bg-side-logo p-0 h-20 text-white">
-      <Image
-        src="/images/logo-color.png"
-        alt="Cloud Wallet Logo"
-        width={230}
-        height={100}
-        className="ml-[-10px]"
-      />
+      <Link href="/" className="cursor-pointer">
+        <Image
+          src="/images/logo-color.png"
+          alt="Cloud Wallet Logo"
+          width={230}
+          height={100}
+          className="ml-[-10px]"
+        />
+      </Link>
     </SidebarHeader>
   );
 };
