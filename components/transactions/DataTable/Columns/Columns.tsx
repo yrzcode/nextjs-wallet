1157925b7dd0dfb70aeda7c912bb326609eb7ff9
rz_yang <yrzcode@hotmail.com>
2025-06-22 10:21:23 +0800

    change amount sort function

--- components/transactions/DataTable/Columns/Columns.tsx
+++ components/transactions/DataTable/Columns/Columns.tsx
@@ -10,9 +10,7 @@ import type { Transaction } from "@prisma/client";
 const TableColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "type",
-    header: ({ column }) => (
-      <ColumnHeader title="Type" column={column} sortable />
-    ),
+    header: ({ column }) => <ColumnHeader title="Type" column={column} />,
     size: 100,
   },
   {
@@ -21,6 +19,18 @@ const TableColumns: ColumnDef<Transaction>[] = [
       <ColumnHeader title="Amount" column={column} sortable />
     ),
     cell: AmountCell,
+    sortingFn: (rowA, rowB, columnId) => {
+      const amountA = Number.parseFloat(rowA.getValue(columnId));
+      const typeA = rowA.getValue("type") as string;
+      const amountB = Number.parseFloat(rowB.getValue(columnId));
+      const typeB = rowB.getValue("type") as string;
+
+      // Convert to actual signed values for sorting
+      const signedAmountA = typeA === "Deposit" ? amountA : -amountA;
+      const signedAmountB = typeB === "Deposit" ? amountB : -amountB;
+
+      return signedAmountA - signedAmountB;
+    },
     size: 120,
   },
   {
