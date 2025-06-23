6be5bba5614ec542b19ab9812daa5bbb711b2810
rz_yang <yrzcode@hotmail.com>
2025-06-24 07:02:07 +0800

    click logo clear filter

--- components/layouts/AppSiderbar/Header.tsx
+++ components/layouts/AppSiderbar/Header.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import useFilterStore from "@/hooks/stores/useFilterStore";
 import { SidebarHeader } from "@/components/ui/sidebar";
 
 const Header = () => {
+  const { setFilter } = useFilterStore();
+
+  const handleLogoClick = () => {
+    setFilter({ startDate: undefined, endDate: undefined });
+  };
+
   return (
     <SidebarHeader className="grid place-items-center  bg-side-logo p-0 h-20 text-white">
-      <Link href="/" className="cursor-pointer">
+      <Link href="/" className="cursor-pointer" onClick={handleLogoClick}>
         <Image
           src="/images/logo-color.png"
           alt="Cloud Wallet Logo"
